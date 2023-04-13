6a74d5a782438c3279f18eca1a3c1dfe58162cbc
david <drdelao@cps.edu>
2023-04-13 13:34:09 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Delao, David R on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
